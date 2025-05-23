852de44e8a3dfbad4ee1873cc557c74cddddf3ee
vishnu-suderson <m.vishnusuderson@gmail.com>
2025-05-23 19:02:05 +0530

    add some changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/weather-app/', // Replace with your repo name
   plugins: [react()],
+   base: '/weather-app/', // Replace with your repo name
 })
